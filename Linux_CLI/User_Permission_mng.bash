02dc9605d2f7e952a995f2ceba58f8f9bdfc1e10
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-08-30 21:47:45 +0530

    Create User_Permission_mng.bash

---
+++ Linux_CLI/User_Permission_mng.bash
@@ -0,0 +1,9 @@
+whoami                   # Show current user
+id                       # Show user ID & groups
+adduser devops           # Add new user
+passwd devops            # Change password
+su - devops              # Switch user
+groups                   # Show groups
+chmod 755 file.sh        # Change permissions
+chown user:group file    # Change ownership
+sudo command             # Run command as admin
