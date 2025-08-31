ea8c00ecd6c97141ba71f22ddb7e810488e258b6
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-08-31 20:28:09 +0530

    Create 5_Package_Management.bash

---
+++ Linux_CLI/5_Package_Management.bash
@@ -0,0 +1,5 @@
+sudo apt-get update               # Update repo list
+sudo apt-get upgrade              # Upgrade all packages
+sudo apt-get install nginx        # Install package
+sudo apt-get remove nginx         # Remove package
+dpkg -l                           # List installed packages
