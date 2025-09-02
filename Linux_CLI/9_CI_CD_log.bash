6148ecae62e7563bcd68b24043e9d591098d2c38
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-09-02 21:57:56 +0530

    Create 9_CI_CD_log.bash

---
+++ Linux_CLI/9_CI_CD_log.bash
@@ -0,0 +1,5 @@
+crontab -e                        # Edit cron jobs
+crontab -l                        # List cron jobs
+tail -f /var/log/syslog           # View syslog in real-time
+tail -f /var/log/nginx/access.log # Nginx logs
+tail -f /var/log/nginx/error.log  # Error logs
