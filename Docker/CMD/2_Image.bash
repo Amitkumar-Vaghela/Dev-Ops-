168b6e14b3ec87783852b98b7b3f00d3466a5a74
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-10-04 22:26:02 +0530

    Create 2_Image.bash

---
+++ Docker/CMD/2_Image.bash
@@ -0,0 +1,6 @@
+docker images                 # List all images
+docker pull ubuntu             # Download image from Docker Hub
+docker pull nginx:latest       # Pull specific image version
+docker rmi <image_id>          # Remove image
+docker rmi -f <image_id>       # Force remove image
+docker inspect <image_id>      # View detailed info about image
