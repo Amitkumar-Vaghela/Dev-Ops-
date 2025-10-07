b5a6c3beaf56fb17b83adf2a1ea359f8be9fc960
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-10-07 22:25:29 +0530

    Create 7_Cleanup_Commands.bash

---
+++ Docker/CMD/7_Cleanup_Commands.bash
@@ -0,0 +1,4 @@
+docker system prune                 # Remove unused data (containers, networks)
+docker container prune              # Remove stopped containers
+docker image prune                  # Remove unused images
+docker volume prune                 # Remove unused volumes
